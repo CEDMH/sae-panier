8cbecaaca9de1af27d15f2d1ae9945745159be4b
CEDMH <moreaulucien@gmail.com>
2026-05-20 16:21:26 +0200

    Mise en place de la base de l'index client.

---
+++ client-index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
+  <link rel="stylesheet" href="assets/css/custom.css">
+  <title>ACCUEIL</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Bienvenue sur le site</h1>
+        <p>Choisissez un mode de connexion ou inscrivez-vous pour créer un compte.</p>
+    </header>
+
+    <main>
+
+    </main>
+
+    <script src="script.js"></script>
+
+</body>
+
+</html>
