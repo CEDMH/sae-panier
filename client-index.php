b7598d0cef073a6636725a2e6fbd31525d9190cc
CEDMH <moreaulucien777@gmail.com>
2026-05-28 10:40:31 +0200

    Rajout de Fontawesome

--- client-index.php
+++ client-index.php
@@ -2,13 +2,19 @@
 <html lang="fr">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-  <link rel="stylesheet" href="assets/css/pico-main/css/pico.sand.css">
-  <link rel="stylesheet"href="assets/css/custom.css">
-  <title>ACCUEIL</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ACCUEIL</title>
+    <link href="assets/pico-main/css/pico.sand.css" rel="stylesheet">
+
+    <link href="assets/font-awesome/css/fontawesome.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/brands.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/regular.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/solid.css" rel="stylesheet">
+
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <script src="assets/js/script.js"></script>
 </head>
 
 <body>
@@ -24,8 +30,6 @@
             <a href="">Voir plus</a>
         </article>
     </main>
-
-    <script src="script.js"></script>
 
 </body>
 
