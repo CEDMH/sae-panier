d979693cdc5417ca4b5cb655d34c39caa7d15ec8
CEDMH <moreaulucien777@gmail.com>
2026-06-04 19:16:22 +0200

    Répartition des css

--- client-index.php
+++ client-index.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Sansita:ital,wght@0,400;0,700;0,800;0,900;1,400;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/client-index.css" rel="stylesheet">
 
     <script src="assets/javascript/dm-lm.js"></script>
 </head>
