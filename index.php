29ffcf7962b34dbe27ea26cd334a4783c678bbc5
CEDMH <moreaulucien777@gmail.com>
2026-06-04 18:23:16 +0200

    Rajout de la nav dans le index de connexion et le numero carte oublié

--- index.php
+++ index.php
@@ -40,15 +40,47 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="fr">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@1.*/css/pico.min.css">
-  <link rel="stylesheet" href="assets/css/custom.css">
-  <title>Se connecter</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SE CONNECTER</title>
+    <link href="assets/pico-main/css/pico.sand.css" rel="stylesheet">
+
+    <link href="assets/font-awesome/css/fontawesome.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/brands.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/regular.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/solid.css" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sansita:ital,wght@0,400;0,700;0,800;0,900;1,400;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <script src="assets/javascript/dm-lm.js"></script>
 </head>
 
 <body>
+
+    <nav class="header">
+    <ul>
+      <li><img src="assets/image/logo.png" id="logo"></li>
+    </ul>
+    <ul>
+      <li class="lecoindessaveurs"><img src="assets/image/titre.png" id="titre"></li>
+    </ul>
+    <ul id="menu-burger">
+      <li>
+        <details class="dropdown">
+          <summary>
+            
+          </summary>
+            <ul dir="rtl">
+              <li><button onclick="modeJour()"><i class="fa-solid fa-sun"></i></button> <button onclick="modeNuit()"><i class="fa-solid fa-moon"></i></button></li>
+            </ul>
+        </details>
+      </li>
+    </ul>
+  </nav>
 
     <header>
         <h1>Bienvenue sur le site</h1>
@@ -81,7 +113,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </section>
 
     </main>
-    <script src="script.js"></script>
 </body>
 
 </html>
