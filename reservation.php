516766a670ecab0ce8e24ad16bf08df3b9417fcf
CEDMH <moreaulucien777@gmail.com>
2026-06-11 18:53:11 +0200

    changes que j'ai fait

--- reservation.php
+++ reservation.php
@@ -65,7 +65,7 @@ if (!(!empty($_SESSION['client_carte']) || (!empty($_SESSION['client_nom']) && !
               ?>
 
               <li class="case"><a href="panier.php">Panier</a></li>
-              <li class="deco"><a href="./back/deconnexion.php" class="texticon">Déco <i class="fa-solid fa-power-off"></i></a></li>
+              <li class="deco"><a href="./back/deconnexion.php">Déconnexion</a></li>
               <!-- LES TROIS BOUTONS DU NIGHT MOD, LIGHT MOD ET DYSLEXIC MOD -->
               <li><button onclick="modeJour()"><i class="fa-solid fa-sun"></i></button> <button onclick="modeNuit()"><i class="fa-solid fa-moon"></i></button> <button onclick="modeDys()"><i class="fa-solid fa-universal-access"></i></button></li>
             </ul>
